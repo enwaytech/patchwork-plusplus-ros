f95a6f0667109aea052759eb5d176f9021e3f119
Bo Chen <bo@enway.ai>
2023-08-07 10:20:23 +0200

    reduce log spam and update visualization

--- src/demo.cpp
+++ src/demo.cpp
@@ -2,10 +2,10 @@
 // For disable PCL complile lib, to use PointXYZIR
 #define PCL_NO_PRECOMPILE
 
-#include <ros/ros.h>
-#include <signal.h>
-#include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <signal.h>
 
 #include "patchworkpp/patchworkpp.hpp"
 
@@ -19,53 +19,60 @@ ros::Publisher pub_ground;
 ros::Publisher pub_non_ground;
 
 template<typename T>
-sensor_msgs::PointCloud2 cloud2msg(pcl::PointCloud<T> cloud, const ros::Time& stamp, std::string frame_id = "map") {
-    sensor_msgs::PointCloud2 cloud_ROS;
-    pcl::toROSMsg(cloud, cloud_ROS);
-    cloud_ROS.header.stamp = stamp;
-    cloud_ROS.header.frame_id = frame_id;
-    return cloud_ROS;
-}
-
-void callbackCloud(const sensor_msgs::PointCloud2::Ptr &cloud_msg)
+sensor_msgs::PointCloud2
+cloud2msg(pcl::PointCloud<T> cloud, const ros::Time& stamp, std::string frame_id = "map")
 {
-    double time_taken;
-
-    pcl::PointCloud<PointType> pc_curr;
-    pcl::PointCloud<PointType> pc_ground;
-    pcl::PointCloud<PointType> pc_non_ground;
-
-    pcl::fromROSMsg(*cloud_msg, pc_curr);
-
-    PatchworkppGroundSeg->estimate_ground(pc_curr, pc_ground, pc_non_ground, time_taken);
-
-    ROS_INFO_STREAM("\033[1;32m" << "Input PointCloud: " << pc_curr.size() << " -> Ground: " << pc_ground.size() <<  "/ NonGround: " << pc_non_ground.size()
-         << " (running_time: " << time_taken << " sec)" << "\033[0m");
-
-    pub_cloud.publish(cloud2msg(pc_curr, cloud_msg->header.stamp, cloud_msg->header.frame_id));
-    pub_ground.publish(cloud2msg(pc_ground, cloud_msg->header.stamp, cloud_msg->header.frame_id));
-    pub_non_ground.publish(cloud2msg(pc_non_ground, cloud_msg->header.stamp, cloud_msg->header.frame_id));
+  sensor_msgs::PointCloud2 cloud_ROS;
+  pcl::toROSMsg(cloud, cloud_ROS);
+  cloud_ROS.header.stamp = stamp;
+  cloud_ROS.header.frame_id = frame_id;
+  return cloud_ROS;
 }
 
-int main(int argc, char**argv) {
+void
+callbackCloud(const sensor_msgs::PointCloud2::Ptr& cloud_msg)
+{
+  double time_taken;
 
-    ros::init(argc, argv, "Demo");
-    ros::NodeHandle nh;
-    ros::NodeHandle pnh("~");
+  pcl::PointCloud<PointType> pc_curr;
+  pcl::PointCloud<PointType> pc_ground;
+  pcl::PointCloud<PointType> pc_non_ground;
 
-    std::string cloud_topic;
-    pnh.param<string>("cloud_topic", cloud_topic, "/pointcloud");
+  pcl::fromROSMsg(*cloud_msg, pc_curr);
 
-    cout << "Operating patchwork++..." << endl;
-    PatchworkppGroundSeg.reset(new PatchWorkpp<PointType>(&pnh));
+  PatchworkppGroundSeg->estimate_ground(pc_curr, pc_ground, pc_non_ground, time_taken);
 
-    pub_cloud       = pnh.advertise<sensor_msgs::PointCloud2>("cloud", 100, true);
-    pub_ground      = pnh.advertise<sensor_msgs::PointCloud2>("ground", 100, true);
-    pub_non_ground  = pnh.advertise<sensor_msgs::PointCloud2>("nonground", 100, true);
+  ROS_INFO_STREAM_THROTTLE(10.0,
+                           "\033[1;32m"
+                               << "Input PointCloud: " << pc_curr.size() << " -> Ground: " << pc_ground.size()
+                               << "/ NonGround: " << pc_non_ground.size() << " (running_time: " << time_taken << " sec)"
+                               << "\033[0m");
 
-    ros::Subscriber sub_cloud = nh.subscribe(cloud_topic, 100, callbackCloud);
-    
-    ros::spin();
+  pub_cloud.publish(cloud2msg(pc_curr, cloud_msg->header.stamp, cloud_msg->header.frame_id));
+  pub_ground.publish(cloud2msg(pc_ground, cloud_msg->header.stamp, cloud_msg->header.frame_id));
+  pub_non_ground.publish(cloud2msg(pc_non_ground, cloud_msg->header.stamp, cloud_msg->header.frame_id));
+}
 
-    return 0;
+int
+main(int argc, char** argv)
+{
+  ros::init(argc, argv, "Demo");
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+
+  std::string cloud_topic;
+  pnh.param<string>("cloud_topic", cloud_topic, "/pointcloud");
+
+  cout << "Operating patchwork++..." << endl;
+  PatchworkppGroundSeg.reset(new PatchWorkpp<PointType>(&pnh));
+
+  pub_cloud = pnh.advertise<sensor_msgs::PointCloud2>("cloud", 100, true);
+  pub_ground = pnh.advertise<sensor_msgs::PointCloud2>("ground", 100, true);
+  pub_non_ground = pnh.advertise<sensor_msgs::PointCloud2>("nonground", 100, true);
+
+  ros::Subscriber sub_cloud = nh.subscribe(cloud_topic, 100, callbackCloud);
+
+  ros::spin();
+
+  return 0;
 }
